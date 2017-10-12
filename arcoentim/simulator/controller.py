4c8ef010bcef0d04f14babf58a6339f6e99b1a32
timijntema <klimtim@hotmail.com>
2017-10-12 13:05:01 +0200

    Changed the controller to use the new hardware interface and removed the buttons from the controller

--- arcoentim/simulator/controller.py
+++ arcoentim/simulator/controller.py
@@ -1,7 +1,5 @@
-import lemonator
 from tkinter import *
 import gui
-import simulatorGui
 
 class controller():
     userInterface = gui.gui
@@ -15,49 +13,28 @@ class controller():
     userSelectLemonade = True  # True = lemonade, False = water
     userStartMixing = False
 
-    def __init__(self):
-        self.guiBase()
-
-    def guiBase(self):
-        Button(self.userInterface.master, text="1", command=lambda: self.keypadButton(1)).place(x=160, y=300)
-        Button(self.userInterface.master, text="2", command=lambda: self.keypadButton(2)).place(x=210, y=300)
-        Button(self.userInterface.master, text="3", command=lambda: self.keypadButton(3)).place(x=260, y=300)
-        Button(self.userInterface.master, text="4", command=lambda: self.keypadButton(4)).place(x=160, y=330)
-        Button(self.userInterface.master, text="5", command=lambda: self.keypadButton(5)).place(x=210, y=330)
-        Button(self.userInterface.master, text="6", command=lambda: self.keypadButton(6)).place(x=260, y=330)
-        Button(self.userInterface.master, text="7", command=lambda: self.keypadButton(7)).place(x=160, y=360)
-        Button(self.userInterface.master, text="8", command=lambda: self.keypadButton(8)).place(x=210, y=360)
-        Button(self.userInterface.master, text="9", command=lambda: self.keypadButton(9)).place(x=260, y=360)
-        Button(self.userInterface.master, text="0", command=lambda: self.keypadButton(0)).place(x=210, y=390)
-        Button(self.userInterface.master, text="*", command=lambda: self.keypadButton('*')).place(x=160, y=390)
-        Button(self.userInterface.master, text="#", command=lambda: self.keypadButton('#')).place(x=260, y=390)
-        Button(self.userInterface.master, text="A", command=lambda: self.keypadButton('A')).place(x=310, y=300)
-        Button(self.userInterface.master, text="B", command=lambda: self.keypadButton('B')).place(x=310, y=330)
-        Button(self.userInterface.master, text="C", command=lambda: self.keypadButton('C')).place(x=310, y=360)
-        Button(self.userInterface.master, text="D", command=lambda: self.keypadButton('D')).place(x=310, y=390)
-
-
     def keypadButton(self, buttonValue):
-        ## needs to be changed to setc() so the chars can be retreived with getc()
-        correspondingActions = {1: lambda: self.assignDrinkValues("1"),
-                                2: lambda: self.assignDrinkValues("2"),
-                                3: lambda: self.assignDrinkValues("3"),
-                                4: lambda: self.assignDrinkValues("4"),
-                                5: lambda: self.assignDrinkValues("5"),
-                                6: lambda: self.assignDrinkValues("6"),
-                                7: lambda: self.assignDrinkValues("7"),
-                                8: lambda: self.assignDrinkValues("8"),
-                                9: lambda: self.assignDrinkValues("9"),
-                                0: lambda: self.assignDrinkValues("0"),
-                                '*': lambda: setattr(self.sim,"iscupPresent", "No"), # Take the cup away from the simulator
-                                '#': lambda: setattr(self.sim,"iscupPresent", "Yes"), # Put the cup back into the simulator
-                                'A': lambda: setattr(self.sim,"userSelectLemonade", False), # Select water amount
-                                'B': lambda: setattr(self.sim,"userSelectLemonade", True), # Select lemonade amount
-                                'C': lambda: setattr(self.sim,"userStartMixing", True), # Start mixing
-                                'D': lambda: print('D')}
+        if buttonValue != "":
+            ## needs to be changed to setc() so the chars can be retreived with getc()
+            correspondingActions = {1: lambda: self.assignDrinkValues("1"),
+                                    2: lambda: self.assignDrinkValues("2"),
+                                    3: lambda: self.assignDrinkValues("3"),
+                                    4: lambda: self.assignDrinkValues("4"),
+                                    5: lambda: self.assignDrinkValues("5"),
+                                    6: lambda: self.assignDrinkValues("6"),
+                                    7: lambda: self.assignDrinkValues("7"),
+                                    8: lambda: self.assignDrinkValues("8"),
+                                    9: lambda: self.assignDrinkValues("9"),
+                                    0: lambda: self.assignDrinkValues("0"),
+                                    '*': lambda: self.__setattr__("iscupPresent", "No"), # Take the cup away from the simulator
+                                    '#': lambda: self.__setattr__("iscupPresent", "Yes"), # Put the cup back into the simulator
+                                    'A': lambda: self.__setattr__("userSelectLemonade", False), # Select water amount
+                                    'B': lambda: self.__setattr__("userSelectLemonade", True), # Select lemonade amount
+                                    'C': lambda: self.__setattr__("userStartMixing", True), # Start mixing
+                                    'D': lambda: print('D')}
 
-        ## Execute watever the lambda function needs to
-        correspondingActions[buttonValue]()
+            ## Execute watever the lambda function needs to
+            correspondingActions[buttonValue]()
 
     def assignDrinkValues(self, value):
         if self.userSelectLemonade:
@@ -67,51 +44,52 @@ class controller():
 
     def updateLabels(self):
         ## Change variables
+        self.hwInterface.emptyLcd()
+        self.keypadButton(self.hwInterface.getc("keypad"))
+
+        ## Check if a cup is in the machine
         if not self.hwInterface.get("isCupPresent"):
             self.hwInterface.set("sirupPump", 0)
             self.hwInterface.set("sirupValve", 1)
             self.hwInterface.set("waterPump", 0)
             self.hwInterface.set("waterValve", 1)
             self.hwInterface.putSting("lcd", "Plz put a cup in the machine")
+
+        ## If the cup is present start checking some other things
         elif self.hwInterface.get("isCupPresent"):
             if self.userStartMixing:
-                self.lcd = "Pouring your drink\nSelected amounts Water: " + str(
-                    int(self.userWaterValue)) + " Lemonade: " + str(int(self.userLemonadeValue))
-                self.waterLevel += 1
-                if self.waterLevel < int(self.userLemonadeValue):
-                    self.lcd += "\nNow pouring: lemonade"
-                    self.sirupValveValue = 0
-                    self.sirupPumpValue = 1
-                    self.waterPumpValue = 0
-                    self.waterValveValue = 1
+                self.hwInterface.putSting("lcd", "Pouring your drink\nSelected amounts Water: " + str(int(self.userWaterValue)) + " Lemonade: " + str(int(self.userLemonadeValue)))
+                if self.hwInterface.read_mm() < int(self.userLemonadeValue):
+                    self.hwInterface.putSting("lcd", "\nNow pouring: lemonade")
+                    self.hwInterface.set("sirupPump", 1)
+                    self.hwInterface.set("sirupValve", 0)
+                    self.hwInterface.set("waterPump", 0)
+                    self.hwInterface.set("waterValve", 1)
 
-                elif self.waterLevel >= int(self.userLemonadeValue) and self.waterLevel < int(
-                        self.userWaterValue) + int(self.userLemonadeValue):
-                    self.sirupValveValue = 1
-                    self.sirupPumpValue = 0
-                    self.waterPumpValue = 1
-                    self.waterValveValue = 0
-                    self.lcd += "\nNow pouring: water"
+                elif self.hwInterface.read_mm() >= int(self.userLemonadeValue) and self.hwInterface.read_mm() < int(self.userWaterValue) + int(self.userLemonadeValue):
+                    self.hwInterface.set("sirupPump", 0)
+                    self.hwInterface.set("sirupValve", 1)
+                    self.hwInterface.set("waterPump", 1)
+                    self.hwInterface.set("waterValve", 0)
+                    self.hwInterface.putSting("lcd", "\nNow pouring: water")
+
                 else:
-                    self.lcd = "Drink has been poured"
-                    self.sirupValveValue = 1
-                    self.sirupPumpValue = 0
-                    self.waterPumpValue = 0
-                    self.waterValveValue = 1
+                    self.hwInterface.putString("lcd", "Drink has been poured")
+                    self.hwInterface.set("sirupPump", 0)
+                    self.hwInterface.set("sirupValve", 1)
+                    self.hwInterface.set("waterPump", 0)
+                    self.hwInterface.set("waterValve", 1)
                     self.userStartMixing = False
                     self.userWaterValue = "0"
                     self.userLemonadeValue = "0"
-                    # changin weglaten
 
             elif not self.userStartMixing:
-                self.lcd = "Waiting for start(C) or values(A, B)\nSelected amounts Water: " + str(
-                    int(self.userWaterValue)) + " Lemonade: " + str(int(self.userLemonadeValue))
-                self.sirupPumpValue = 0
-                self.sirupValveValue = 1
-                self.waterPumpValue = 0
-                self.waterValveValue = 1
-                self.waterLevel = 0
+                self.hwInterface.putSting("lcd", "Waiting for start(C) or values(A, B)\nSelected amounts Water: " + str(int(self.userWaterValue)) + " Lemonade: " + str(int(self.userLemonadeValue)))
+                self.hwInterface.set("sirupPump", 0)
+                self.hwInterface.set("sirupValve", 1)
+                self.hwInterface.set("waterPump", 0)
+                self.hwInterface.set("waterValve", 1)
                 if self.userSelectLemonade:
-                    self.lcd += "\nChanging lemonade now"
+                    self.hwInterface.putSting("lcd", "\nChanging lemonade now")
                 else:
-                    self.lcd += "\nChanging water now"
+                    self.hwInterface.putSting("lcd", "\nChanging water now")
