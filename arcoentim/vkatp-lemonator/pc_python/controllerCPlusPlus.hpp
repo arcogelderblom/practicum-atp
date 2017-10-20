ad2b9482df403be794ff9171ab89fbddc02a9041
timijntema <klimtim@hotmail.com>
2017-10-20 13:40:21 +0200

    Fixed a few issues with the creation of variables

--- arcoentim/vkatp-lemonator/pc_python/controllerCPlusPlus.hpp
+++ arcoentim/vkatp-lemonator/pc_python/controllerCPlusPlus.hpp
@@ -14,7 +14,7 @@ namespace py = pybind11;
 
 class controllerCPlusPlus {
 private:
-    py::object hwInterface = sharedVariables()
+    //py::object hwInterface = sharedVariables()
     
     std::string userLemonadeValue = "0";
     std::string userWaterValue = "0";
@@ -23,24 +23,24 @@ private:
     int originalDistance = 100;
     int currentLevel = 0;
     
-    std::map buttonActions<char, std::function<void()>> = {
-        '1' : [&](){ assignDrinkValues('1') },
-        '2' : [&](){ assignDrinkValues('2') },
-        '3' : [&](){ assignDrinkValues('3') },
-        '4' : [&](){ assignDrinkValues('4') },
-        '5' : [&](){ assignDrinkValues('5') },
-        '6' : [&](){ assignDrinkValues('6') },
-        '7' : [&](){ assignDrinkValues('7') },
-        '8' : [&](){ assignDrinkValues('8') },
-        '9' : [&](){ assignDrinkValues('9') },
-        '0' : [&](){ assignDrinkValues('0') },
-        '*' : [&](){ hwInterface.set("isCupPresent", false) },
-        '#' : [&](){ hwInterface.set("isCupPresent", true)  },
-        'A' : [&](){ userSelectLemonade = false },
-        'B' : [&](){ userSelectLemonade = true },
-        'C' : [&](){ userStartMixing = true },
-        'D' : [&](){ }
-    };
+    std::map<char, std::function<void()>> buttonActions;
+    
+    //buttonActions['1'] = [&](){ assignDrinkValues('1') };
+    //buttonActions['2'] = [&](){ assignDrinkValues('2') };
+    //buttonActions['3'] = [&](){ assignDrinkValues('3') };
+    //buttonActions['4'] = [&](){ assignDrinkValues('4') };
+    //buttonActions['5'] = [&](){ assignDrinkValues('5') };
+    //buttonActions['6'] = [&](){ assignDrinkValues('6') };
+    //buttonActions['7'] = [&](){ assignDrinkValues('7') };
+    //buttonActions['8'] = [&](){ assignDrinkValues('8') };
+    //buttonActions['9'] = [&](){ assignDrinkValues('9') };
+    //buttonActions['0'] = [&](){ assignDrinkValues('0') };
+    //buttonActions['*'] = [&](){ hwInterface.set("isCupPresent", false) };
+    //buttonActions['#'] = [&](){ hwInterface.set("isCupPresent", true)  };
+    //buttonActions['A'] = [&](){ userSelectLemonade = false };
+    //buttonActions['B'] = [&](){ userSelectLemonade = true };
+    //buttonActions['C'] = [&](){ userStartMixing = true };
+    //buttonActions['D'] = [&](){ };
     
 public:
     controllerCPlusPlus();
